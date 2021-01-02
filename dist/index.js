a3b32d2730658ba9a599bcc7796a2dea1bd9c8ff
cristian3457 <s16120279@alumnos.itsur.edu.mx>
2021-01-01 19:20:43 -0600

    Se agregarón las rutas para usuarios

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,13 @@ const mysql_1 = __importDefault(require("./mysql/mysql"));
 const body_parser_1 = __importDefault(require("body-parser"));
 let puerto = process.env.PORT || 3000;
 const server = server_1.default.init(puerto);
+// CORS
+server.app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
 // Body Parser
 // parse application/x-www-form-urlencoded
 server.app.use(body_parser_1.default.urlencoded({ extended: false }));
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,14 @@ let puerto:any = process.env.PORT || 3000;
 
 const server = Server.init(puerto);
 
+// CORS
+server.app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
 // Body Parser
 
 // parse application/x-www-form-urlencoded
